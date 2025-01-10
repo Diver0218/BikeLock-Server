c094588c010ed11c174af9d9e0ba756da638a5cb
Diver0218 <jamin.bareis@gmail.com>
2025-01-10 10:57:35 +0100

    updated django structure

--- WebBluetooth/apps.py
+++ WebBluetooth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ClientauthConfig(AppConfig):
+class WebbluetoothConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ClientAuth'
+    name = 'WebBluetooth'
