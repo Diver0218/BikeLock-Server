1fdfc52fe8dc5185ced912e61ba3254c70e71fab
Diver0218 <jamin.bareis@gmail.com>
2025-01-15 13:39:36 +0100

    added timestampt for token to Lock Model

--- LockAuth/models.py
+++ LockAuth/models.py
@@ -4,5 +4,7 @@ import uuid
 class Lock(models.Model):
     
     LockID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    PermittedUsers = models.ManyToManyField('User', related_name='Locks')
+    CurrentToken = models.CharField(max_length=32, default='')
+    TokenTimestamp = models.DateTimeField()
+    # PermittedUsers = models.ManyToManyField('auth.UserManager', related_name='Locks')
     
--- LockAuth/views.py
+++ LockAuth/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 from django.views.generic import View
 from .scripts.auth import authenticate
 from django.http import JsonResponse
+import secrets
+import datetime
+from models import Lock
 
 class AuthView(View):
     
     def get(self, request, *args, **kwargs):
+        now_time = datetime.datetime.now()
+        token = secrets.token_hex(32)
+        LockID = reuquest.GET.get('LockID')
+        
+        Lock.objects.update_or_create(LockID=1, defaults={'CurrentToken': token, 'TokenTimestamp': now_time})
+        
+        
+    def post(self, request, *args, **kwargs):
         token = request.GET.get('token')
         if authenticate(token):
             return JsonResponse(status=200)
