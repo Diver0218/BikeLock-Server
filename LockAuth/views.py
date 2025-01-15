67857c9e3f4039a53c16b42add067ca0116bf715
Diver0218 <jamin.bareis@gmail.com>
2025-01-15 10:45:23 +0100

    setup urls routing and method strcture for auth

--- LockAuth/views.py
+++ LockAuth/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View
+from scripts.auth import authenticate
+from django.http import JsonResponse
 
-# Create your views here.
+class AuthView(View):
+    
+    def get(self, request, *args, **kwargs):
+        token = request.GET.get('token')
+        if authenticate(token):
+            return JsonResponse(status=200)
+        else:
+            return JsonResponse(status=401)
