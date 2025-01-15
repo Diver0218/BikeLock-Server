1a65ca7c090039748fe1d6bd6f04131fca378dbd
Diver0218 <jamin.bareis@gmail.com>
2025-01-15 10:45:38 +0100

    crreated Lock Model

--- LockAuth/models.py
+++ LockAuth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Lock(models.Model):
+    
+    LockID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    PermittedUsers = models.ManyToManyField('User', related_name='Locks')
+    
