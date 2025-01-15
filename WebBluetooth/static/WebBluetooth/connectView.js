1d3b2c51a6229b40ac7a046ce442af8a3f7874b4
Diver0218 <jamin.bareis@gmail.com>
2025-01-15 11:28:58 +0100

    added feature to get token via HTTP and send it on GATT

--- WebBluetooth/static/WebBluetooth/connectView.js
+++ WebBluetooth/static/WebBluetooth/connectView.js
@@ -1,3 +1,5 @@
+const url = "example.com"
+
 function connectDevice() {
     navigator.bluetooth.requestDevice(
         {
@@ -18,13 +20,31 @@ function connectDevice() {
             return service.getCharacteristic('your_characteristic_uuid');
         })
         .then(characteristic => {
-            const data = new Uint8Array([1, 2, 3]); // Example data to send
-            return characteristic.writeValue(data);
-        })
-        .then(() => {
-            console.log('Value written successfully');
+            sendToken(characteristic);
         })
         .catch(error => {
             console.error('Error:', error);
         });
+}
+
+function sendToken(characteristic)
+{
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ data }) // Convert data to JSON string
+    };
+
+    let response = fetch(url, options);
+    if (response.ok)
+    {
+        let data = response.body.json()
+        characteristic.writeValue(data.token);
+    }
+    else
+    {
+        throw new Error('Request Failed');
+    }
 }
