a8083276110dc3e6c1e504a1053256e0a36794e8
Diver0218 <jamin.bareis@gmail.com>
2025-01-15 10:38:08 +0100

    added LockAuth App

--- BikeLock/urls.py
+++ BikeLock/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('WebBluetooth', include('WebBluetooth.urls')),
+    path('WebBluetooth/', include('WebBluetooth.urls')),
+    path('LockAuth/', include('LockAuth.urls')),
 ]
