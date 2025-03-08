70cce9807b3b5b3d70344ce00c146e6869fa77da
Diver0218 <jamin.bareis@gmail.com>
2025-03-08 16:23:35 +0000

    Modified to work with nginx

--- BikeLock/settings.py
+++ BikeLock/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,10 +27,7 @@ SECRET_KEY = 'django-insecure-ip(72$i1gscp7itqw=6vai9__lo^+$3v*eur%9r*gwe&(aqhl&
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "192.168.178.49",
-    "pi.local",
+    "www.jamin.chickenkiller.com",
     "jamin.chickenkiller.com",
 ]
 
@@ -127,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
